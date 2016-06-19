084ff740924a7f1005ef51a72d00f3c0d958ef44
Zachary Golba <zachary.golba@postlight.com>
2016-06-19 17:01:57 -0400

    feat: add flowtype support plugins

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ module.exports = {
     require('babel-plugin-transform-es2015-parameters'),
     require('babel-plugin-transform-es2015-spread'),
     require('babel-plugin-transform-exponentiation-operator'),
-    require('babel-plugin-transform-object-rest-spread')
+    require('babel-plugin-transform-object-rest-spread'),
+    require('babel-plugin-syntax-flow'),
+    require('babel-plugin-transform-flow-strip-types')
   ]
 };
