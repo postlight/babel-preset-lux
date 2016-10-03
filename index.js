ab29f58746cf930336ec5c8e7ab3928a29664a47
Zachary Golba <zachary.golba@postlight.com>
2016-10-03 17:20:17 -0400

    refactor: do not specify unecessary syntax plugins

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 module.exports = {
   plugins: [
     require('babel-plugin-external-helpers'),
-    require('babel-plugin-syntax-trailing-function-commas'),
     require('babel-plugin-transform-async-to-generator'),
     require('babel-plugin-transform-class-properties'),
     require('babel-plugin-transform-es2015-destructuring'),
@@ -9,7 +8,6 @@ module.exports = {
     require('babel-plugin-transform-es2015-spread'),
     require('babel-plugin-transform-exponentiation-operator'),
     require('babel-plugin-transform-object-rest-spread'),
-    require('babel-plugin-syntax-flow'),
     require('babel-plugin-transform-flow-strip-types')
   ]
 };
