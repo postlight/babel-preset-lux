a513b003b45b44e4fec2a1ecfa4638d6041f0795
Zachary Golba <zachary.golba@postlight.com>
2016-12-15 20:24:41 -0500

    feat: use babili preset in production mode

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const IS_NODE_SEVEN = process.version.charAt(1) === '7';
 const HAS_HARMONY_FLAG = process.execArgv.includes('--harmony');
 
+const presets = [];
 const plugins = [
   require('babel-plugin-external-helpers'),
   require('babel-plugin-transform-class-properties'),
-  require('babel-plugin-transform-es2015-destructuring'),
-  require('babel-plugin-transform-es2015-parameters'),
-  require('babel-plugin-transform-es2015-spread'),
-  require('babel-plugin-transform-exponentiation-operator'),
-  require('babel-plugin-transform-object-rest-spread'),
-  require('babel-plugin-transform-flow-strip-types')
+  require('babel-plugin-transform-flow-strip-types'),
+  [require('babel-plugin-transform-object-rest-spread'), {
+    useBuiltIns: true
+  }]
 ];
+
+if (process.env.NODE_ENV === 'production') {
+  presets.push(require('babel-preset-babili'));
+}
 
 if (!IS_NODE_SEVEN || !HAS_HARMONY_FLAG) {
   plugins.push(require('babel-plugin-transform-async-to-generator'));
 }
 
 module.exports = {
-  plugins: plugins
+  presets,
+  plugins
 };
